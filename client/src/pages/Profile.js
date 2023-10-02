ce70908364f3edf9d38c7c840eb8d5320ddf82ad
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-10-02 09:14:21 -0600

    profile page gets updated

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import { useQuery, useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom'; //we use Link to navigate other pages
+import { Link } from 'react-router-dom';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_DIY } from '../utils/mutations';
 
-// Import queries and mutations
-import { GET_SAVED_DIYS } from '../utils/queries';
-import { REMOVE_SAVED_DIY } from '../utils/mutations';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
+import { SiQuicklook } from 'react-icons/si';
 
-function SavedDIY({ userId }) {
-  const { loading, error, data } = useQuery(GET_SAVED_DIYS, {
-    variables: { userId },
-  });
-  const [removeSavedDIYMutation] = useMutation(REMOVE_SAVED_DIY); // Mutation to remove a saved DIY
+import profile from '../images/profile.png';
+import SavedDIY from './savedDIY';
 
-  if (loading) return <div className="text-center py-8">Loading...</div>;
-  if (error) return <div className="text-center py-8">Error! {error.message}</div>;
+function Profile() {
+  const { loading, error, data } = useQuery(GET_ME);
+  const [deleteDIYMutation] = useMutation(REMOVE_DIY);
 
-  const savedDIYs = data.getSavedDIYs;
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error! {error.message}</div>;
 
-  // Function to handle removing a saved DIY
-  const handleRemoveSavedDIY = async (diyId) => {
+  const user = data.me; //user  is the logged in user and access the getMe query
+
+  const handleDeleteDIY = async (diyId) => {
     try {
-      await removeSavedDIYMutation({ 
-        variables: { DIYId: diyId }, // Pass the DIY's id to the mutation
-        refetchQueries: [{ query: GET_SAVED_DIYS, variables: { userId } }], // Refetch the user data after deletion this makes the page update automatically
+      await deleteDIYMutation({
+        variables: { DIYId: diyId },
+        refetchQueries: [{ query: GET_ME }], // Refetch the user data after deletion
       });
     } catch (error) {
       console.error(error);
@@ -30,49 +31,58 @@ function SavedDIY({ userId }) {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <div className="border-b border-t m-5 p-2 border-gray-500">
-        <h3 className="text-3xl font-semibold text-yellow-500 text-center">My Saved DIYs</h3>
-      </div>
-      {savedDIYs.length === 0 ? ( // Check if there are no saved DIYs
-        <p className="text-center text-gray-500 mt-4">No saved DIYs yet.</p>
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-6 mt-4 ml-4 mr-4">
-          {savedDIYs.map((diy) => (
-            <div
-              key={diy._id}
-              className="border rounded-lg shadow-md overflow-hidden" style={{ width: '100%', height: '100%' }}
-            >
-              {/* DIY content */}
-              <div className="p-4">
-                <h4 className="text-gray-500 text-lg font-semibold text-center">{diy.title}</h4>
-              </div>
-              {/* DIY image */}
-              <div className="flex justify-center border-t">
-                <img
-                  src={diy.images[0]}
-                  alt="DIY"
-                  className="object-cover w-full h-40"
-                />
-              </div>
-              {/* View DIY */}
-              <Link to={`/diy/${diy._id}`} className="block p-4 hover:bg-gray-900">
-                <h3 className="text-lg text-yellow-500 hover:text-yellow-600 font-semibold mb-2 text-center underline">View</h3>
-              </Link>
-              <div className="flex justify-between p-4 border-t border-gray-300">
-                <button
-                  className="text-red-500 hover:text-red-600"
-                  onClick={() => handleRemoveSavedDIY(diy._id)}
-                >
-                  Remove from saved
-                </button>
-              </div>
-            </div>
-          ))}
+    // a minimum height of the screen and padding of 8
+    <div className="min-h-screen p-8"> 
+      <div className="max-w-4xl mx-auto bg-gray-900 rounded-lg p-8">
+        <div className="flex flex-col items-center">
+          <div className="rounded-full w-32 h-32 overflow-hidden">
+            <img src={profile} alt="Profile" className="object-cover w-full h-full" />
+          </div>
+          <h2 className="text-2xl font-semibold mt-4">My Profile</h2>
+          <p className="text-gray-500">Username: {user.username}</p>
+          <p className="text-gray-500">Email: {user.email}</p>
         </div>
-      )}
+
+        <div className="mt-8">
+          <div className="border-b border-t m-5 p-2 border-gray-500">
+            <h3 className="text-3xl font-semibold text-yellow-500 text-center">My DIYs</h3>
+          </div>
+          {user.DIYs.length === 0 ? (
+            <p className="text-center text-gray-500 mt-4">No DIYs yet.</p>
+          ) : (
+            // Render the DIYs using grid cols for any screen size
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mt-4">
+            
+              {user.DIYs.map((diy) => (
+                <div key={diy._id} className="bg-bl border rounded-lg shadow-md overflow-hidden" style={{ width: '100%', height: '100%' }}>
+                  <img src={diy.images[0]} alt="my DIY" className="object-cover w-full h-40" />
+                 
+                  <div className="p-4">
+                    <h4 className="text-lg font-semibold">{diy.title}</h4>
+                  </div>
+                  {/* a link to the detail page usin DIY id */}
+                  <div className="flex justify-between p-4 border-t border-gray-300">
+                    <Link to={`/diy/${diy._id}`} className="block p-4 hover:bg-gray-900">
+                      <SiQuicklook size={24} className="text-green-700" />
+                    </Link>
+                    {/* button */}
+                    <button
+                      className="block p-4 hover:bg-gray-900"
+                      onClick={() => handleDeleteDIY(diy._id)}>
+                      <RiDeleteBin5Fill size={24} className="text-red-900" />
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="mt-8">
+        <SavedDIY />
+      </div>
     </div>
   );
 }
 
-export default SavedDIY;
+export default Profile;
